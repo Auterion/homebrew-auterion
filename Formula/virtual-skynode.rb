bb95fde7501481c375faef450b28dc39e618ce00
Matteo Michele Piazzolla <matteo@auterion.com>
2024-11-04 11:50:52 +0100

    update virtual-skynode to 0.0.15

--- Formula/virtual-skynode.rb
+++ Formula/virtual-skynode.rb
@@ -1,20 +1,37 @@
 class VirtualSkynode < Formula
-    desc "Virtual Skynode simulates an Auterion Skynode for development purposes"
-    homepage "https://auterion.com"
-    url "https://dl.cloudsmith.io/public/auterion/public/raw/versions/0.0.10/virtual-skynode.tar.gz"
-    sha256 "7355ea7d11adc8093b5938ed900a2d9d693ef8ada9fe15cd0db038fe1648dca7"
-    version "0.0.10" 
-  
-    depends_on "qemu"
-    depends_on "python3"
-  
-    def install
-      bin.install "virtual-skynode"
-      bin.install "virtual-skynode-convert-rootfs"
-        # Create the share/virtual-skynode directory
-        (share/"virtual-skynode").mkpath
+  include Language::Python::Virtualenv
 
-        # Install the bzImage into the created directory
-        (share/"virtual-skynode").install "bzImage"
-    end
+  desc "Virtual Skynode simulates an Auterion Skynode for development purposes"
+  homepage "https://auterion.com"
+  url "https://dl.cloudsmith.io/public/auterion/public/raw/versions/0.0.15/virtual-skynode.tar.gz"
+  sha256 "c3099f525b43985247e2e458b6683ef1ed79fccff69c19cb54fc74c9e9da7aa2"
+  version "0.0.15"
+
+  depends_on "qemu"
+  depends_on "python3"
+
+  resource "semver" do
+    url "https://files.pythonhosted.org/packages/41/6c/a536cc008f38fd83b3c1b98ce19ead13b746b5588c9a0cb9dd9f6ea434bc/semver-3.0.2.tar.gz"
+    sha256 "6253adb39c70f6e51afed2fa7152bcd414c411286088fb4b9effb133885ab4cc"
   end
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    venv.pip_install resource("semver")
+  
+    # Manually copy the necessary binaries to the virtual environment
+    libexec_bin = libexec/"bin"
+    libexec_bin.install "virtual-skynode"
+    libexec_bin.install "virtual-skynode-convert-rootfs"
+  
+    # Create wrapper scripts to ensure the virtual environment is activated
+    (bin/"virtual-skynode").write_env_script libexec_bin/"virtual-skynode", PATH: "#{libexec_bin}:$PATH"
+    (bin/"virtual-skynode-convert-rootfs").write_env_script libexec_bin/"virtual-skynode-convert-rootfs", PATH: "#{libexec_bin}:$PATH"
+  
+    # Create the share/virtual-skynode directory
+    (share/"virtual-skynode").mkpath
+  
+    # Install the bzImage into the created directory
+    (share/"virtual-skynode").install "bzImage"
+  end
+end
